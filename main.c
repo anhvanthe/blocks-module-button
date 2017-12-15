0fec1620ed20dea5f6678c798790bf8eab11ba46
Karl Taylor <karl@chooseblocks.com>
2017-12-15 01:10:37 +0000

    Module setup

--- main.c
+++ main.c
@@ -12,11 +12,12 @@ void blocks_main (void) {
 }
 
 const vendor_module_info_t blocks_module_info = { 
-	.label    = u"Example Module", 
+	.label    = u"Button Module", 
 	.vendorID = u"Blocks Wearables Ltd.", 
 	.modelID  = {0x12, 0x34, 0x56, 0x78} 
 };
-	
+
+/* Register "get info" dummy function to indicate button-press capability */
 const vendor_array_handler_t blocks_module_functions = { .count = 0, {
-	/* Add standard function handlers here, and update the count value */
+	{FUNC_BTN_GET_INFO, NULL} /* Will never be invoked */
 }};
